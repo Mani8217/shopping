755ed5cca49113d115c9ae7d4056894f6a958aeb
Mani Shidfar <mreza.shidfar@gmail.com>
2022-12-22 16:38:16 +0100

    shopping-site

---
+++ script.js
@@ -0,0 +1,9 @@
+const product = {
+    apple : {phone:1000 , labtop : 1500 , tablet: 600 , watch : 350} ,
+    samsung : {phone:1000 , labtop : 1500 , tablet: 600 , watch : 350},
+    sony : {phone:1000 , labtop : 1500 , tablet: 600 , watch : 350},
+    lg : {phone:1000 , labtop : 1500 , tablet: 600 , watch : 350},
+    philips : { phone:1000 , labtop : 1500 , tablet: 600 , watch : 350},
+    huawi : {phone:1000 , labtop : 1500 , tablet: 600 , watch : 350}
+};
+document.getElementById('demo').innerHTML = product.apple;
